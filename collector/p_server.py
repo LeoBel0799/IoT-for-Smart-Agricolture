9fef8236c7fddf770bc078dcb02f1c994739313d
leobel99 <leobel99@gmail.com>
2022-11-08 10:25:44 +0100

    Fix

--- collector/p_server.py
+++ collector/p_server.py
@@ -66,7 +66,7 @@ class MqttClient():
         timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         with self.connection.cursor() as cursor:
             # Create a new record
-            sql = "INSERT INTO `mqttsensors` (`temperature`,`humidity`,`forecast`,`pressure`,`water`,`timestamp`) VALUES (%s,%s,%s,%s,%s)"
+            sql = "INSERT INTO mqttsensors (temperature,humidity,forecast,pressure,water,timestamp) VALUES (%s,%s,%s,%s,%s)"
             cursor.execute(sql, (temperature, humidity, forecast, pressure, water, timestamp))
             print("Temperature : ")
             print(temperature)
